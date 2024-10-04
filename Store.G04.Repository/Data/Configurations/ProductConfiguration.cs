c06c48cfa10f382d598fc74d1d228362407e0868
ayanada739 <ayanada739@gmail.com>
2024-10-04 08:33:19 +0300

    Add project files.

---
+++ Store.G04.Repository/Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Store.G04.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G04.Repository.Data.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(P => P.Name).HasMaxLength(maxLength: 200).IsRequired();
+            builder.Property(P => P.PictureUrl).IsRequired(required: true);
+
+            builder.Property(P => P.Price).HasColumnType(typeName: "decimal(18,2)");
+
+            builder.HasOne(P => P.Brand)
+                .WithMany()
+                .HasForeignKey(P => P.BrandId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            builder.HasOne(P => P.Type)
+                .WithMany()
+                .HasForeignKey(P => P.TypeId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            builder.Property(P => P.BrandId).IsRequired(required: false);
+            builder.Property(P => P.TypeId).IsRequired(required: false);
+
+
+        }
+    }
+}
---
+++ Store.G04.Repository/Data/Contexts/StoreDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Store.G04.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G04.Repository.Data.Contexts
+{
+    //CLR
+    public class StoreDbContext : DbContext
+    {
+        public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options)
+        {
+            
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> Brands { get; set; }
+        public DbSet<ProductType> Types { get; set; }
+
+    }
+}
