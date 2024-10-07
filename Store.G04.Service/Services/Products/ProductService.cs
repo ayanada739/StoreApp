0d4123aedb56fd8217f15db2103a83913af1a446
ayanada739 <ayanada739@gmail.com>
2024-10-07 14:35:01 +0300

    Refactoring Prodduct Service And Add Product Spec

--- Store.G04.Service/Services/Products/ProductService.cs
+++ Store.G04.Service/Services/Products/ProductService.cs
@@ -3,6 +3,8 @@ using Store.G04.Core;
 using Store.G04.Core.Dtos.Products;
 using Store.G04.Core.Entities;
 using Store.G04.Core.Sevices.Contract;
+using Store.G04.Core.Specifications;
+using Store.G04.Core.Specifications.Products;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,25 +25,36 @@ namespace Store.G04.Service.Services.Products
             _mapper = mapper;
         }
         public async Task<IEnumerable<ProductDto>> GetAllProductsAsync()
-            =>  _mapper.Map<IEnumerable<ProductDto>>(await _unitOfWork.Repository<Product, int>().GetAllAsync());          
-        
+        {
+            var spec = new ProductSpecifications() ;
+            return _mapper.Map<IEnumerable<ProductDto>>(await _unitOfWork.Repository<Product, int>().GetAllWithSpecAsync(spec));
 
-        public async Task<IEnumerable<TypeBrandDto>> GetAllTypesAsync()
-                   => _mapper.Map<IEnumerable<TypeBrandDto>>(await _unitOfWork.Repository<Product, int>().GetAllAsync());
+        }
 
 
         public async Task<ProductDto> GetProductById(int id)
         {
-           var product = await _unitOfWork.Repository<Product, int>().GetAsync(id);
+            var spec = new ProductSpecifications(id);
+            var product = await _unitOfWork.Repository<Product, int>().GetWithSpecAsync(spec);
             var mappedProduct = _mapper.Map<ProductDto>(product);
             return mappedProduct;
         }
+
         public async Task<IEnumerable<TypeBrandDto>> GetAllBrandsAsync()
         {
             var brands = await _unitOfWork.Repository<ProductBrand, int>().GetAllAsync();
             var mappedBrand = _mapper.Map<IEnumerable<TypeBrandDto>>(brands);
             return mappedBrand;
         }
+
+        public async Task<IEnumerable<TypeBrandDto>> GetAllTypesAsync()
+        {
+           return _mapper.Map<IEnumerable<TypeBrandDto>>(await _unitOfWork.Repository<Product, int>().GetAllAsync());
+
+        }
+
+
+
    
     }
 }
---
+++ StroreG04Core/Specifications/Products/ProductSpecifications.cs
@@ -0,0 +1,28 @@
+﻿using Store.G04.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G04.Core.Specifications.Products
+{
+    public class ProductSpecifications :BaseSpecifications<Product, int>
+    {
+        public ProductSpecifications(int id) : base(P => P.Id == id)
+        {
+            ApplyIncludes();
+        }
+
+        public ProductSpecifications()
+        {
+            ApplyIncludes();
+        }
+
+        private void ApplyIncludes()
+        {
+            Includes.Add(P => P.Brand);
+            Includes.Add(P => P.Type);
+        }
+    }
+}
