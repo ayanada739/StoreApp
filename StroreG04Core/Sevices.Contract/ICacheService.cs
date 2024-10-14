2f3be1ce220015bf31c41bbc1dc68602c39d7fdf
ayanada739 <ayanada739@gmail.com>
2024-10-14 16:13:32 +0300

    Cache Service

---
+++ StroreG04Core/Sevices.Contract/ICacheService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G04.Core.Sevices.Contract
+{
+    public interface ICacheService
+    {
+        Task SetCatcheKeyAsync(string key, object response, TimeSpan expireTime);
+        Task<string> GetCacheKeyAsync(string key);
+
+    }
+}
